7efc5fab28970d7c23fd9b9cee574e0a342b8b8e
Shrutayu Wankhade <shrutayuwankhade1111@gmail.com>
2025-07-13 21:28:40 +0530

    Update gesture detection.py

--- gesture detection.py
+++ gesture detection.py
@@ -1,15 +1,16 @@
+#import libraries
 import cv2
 import numpy as np
 import mediapipe as mp
 import tensorflow as tf
 from tensorflow.keras.models import load_model
 
-
+#
 mpHands = mp.solutions.hands
 hands = mpHands.Hands(max_num_hands=1, min_detection_confidence=0.7)
 mpDraw = mp.solutions.drawing_utils
 
-
+#
 model = load_model('mp_hand_gesture')
 
 f = open('gesture.names', 'r')
@@ -55,7 +56,8 @@ while True:
                    1, (0,0,255), 2, cv2.LINE_AA)
 
     cv2.imshow("Output", frame) 
-
+    
+    #
     if cv2.waitKey(1) == ord('q'):
         break
 
